0f09c5a24ff2f9d04e09b9ed0e64047f4e0235eb
unknown <mcsalazar122@gmail.com>
2022-05-22 13:58:32 -0500

    setup files and starter code and pseudocode

---
+++ assets/jscript.js
@@ -0,0 +1,30 @@
+
+//High scores on left = link to High scores "page"
+//page will be visible for list of high scores only
+//scores are stored in local storage - and recalled when High Scores
+//button is clicked
+//there should also be a return to main button on this page
+
+//timer on right 
+//
+
+//Intro section will show first then disappear
+//buttom to start quiz
+//will cause all but header to disappear
+//will cause 1st set of questions to appear,
+//will cause timer to begin
+
+//Questions section 
+//1 questions with 4 answer button
+//on any buttom click the "response" will appear with right or wrong"
+//a divider line will appear befor response
+//2 seconds later next question/answer will appear and continue
+//until all answers are completed
+
+//each right answer will add value of 10
+//each wrong answer will subtract value of -10
+// total will not go below 0
+
+//after last question, last "page" will appear and show message
+//and score, with opportunity to add initials
+//initials and score will be saved to local storage
