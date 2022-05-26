b64e90538b291cef940dcc8f32c7c95000ef5a3a
unknown <mcsalazar122@gmail.com>
2022-05-25 19:23:28 -0500

    timer and highscore code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # coding-quiz
+
+## Summary
+ This webpage is created to test your JavaScript knowledge!  Using HTML, CSS, and JavaScript I have created a timed quiz.  You have only 1 minute to complete the entire quiz, you will lose time for wrong questions and you will receive your score at the end.  You are able to save you score and initials so you can try again and get a better score.  
--- assets/jscript.js
+++ assets/jscript.js
@@ -1,5 +1,77 @@
+//variables
+var qCounter = 0
+var questions = []
+
+var highScoreEl = document.querySelector("#highscore");
+var timerEl = document.querySelector("#timer");
+// button click to start quiz and to move to next set of questions
+var startBtnEl = document.querySelector("#startBtn");
+var closeIntro = document.querySelector(".intro")
+
+//var startQuiz = document.querySelector("")
+
+var highScoreHandler = function() {
+     var scoreInput = document.querySelector("input[name ='highscore']").value;
+     var initialsInput = document.querySelector("input[name='initials']").value;
+     if (scoreInput === "" || initialsInput == "") {
+         alert("You need to take the quiz to view a high score");
+         return false;
+     }
+ };
+
+ //[WORKS]timer in top right corner
+ var counter = 75
+ var countdown = function() {
+   timerEl.innerHTML = counter;
+   counter--;
+   if(counter === 0) {
+     alert("Time's Up!");
+     clearInterval (startCountdown);
+   };
+ };
+
+ var startCountdown = setInterval(countdown, 1000);
+
+
+//createtask(showQuestions)
+
+//Intro Page (.intro) (button)
+
+//var taskButtonHandler = function() {
+ // get target element from event sample below to start quiz:
+startBtnEl.addEventListener("click", function() {
+  alert("it works!");
+  closeIntro.visible=false;
+  startCountdown
+
+  //showQuestions ()
+});
+
+
+//  var targetEl = event.target;
+
+//  if (targetEl.matches(".edit-btn")) {
+//    console.log("edit", targetEl);
+//    var taskId = targetEl.getAttribute("data-task-id");
+//    editTask(taskId);
+//  } else if (targetEl.matches(".delete-btn")) {
+//    console.log("delete", targetEl);
+//    var taskId = targetEl.getAttribute("data-task-id");
+//    deleteTask(taskId);
+//  }
+
+
+
+
+//reset form fields?? may not be needed
+
+
+
+
 
 //High scores on left = link to High scores "page"
+
+
 //page will be visible for list of high scores only
 //scores are stored in local storage - and recalled when High Scores
 //button is clicked
